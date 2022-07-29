1ce24f34d2ca44caa961133a64ddfd25b54921cc
nhakkaou <noureddine.hak@gmail.com>
2022-07-29 02:10:53 +0100

    score and reflect

--- API/src/Game.js
+++ API/src/Game.js
@@ -22,10 +22,24 @@ let stage = {
     args: [1.5, 1.5, 40],
   },
 };
+let player1 = {
+  position: [0, -60 / 2 + 3, 0],
+  size: 40 / 5,
+  height: 1.5,
+  width: 2,
+};
+let player2 = {
+  position: [0, 60 / 2 - 3, 0],
+  size: 40 / 5,
+  height: 1.5,
+  width: 2,
+};
 class Game {
   constructor() {
     this.ball = ball;
     this.stage = stage;
+    this.player1 = player1;
+    this.player2 = player2;
   }
   newGame(socket) {
     socket.on("newGame", () => {
@@ -34,27 +48,21 @@ class Game {
     });
   }
   ballMove(socket) {
-    let dx = 1;
-    let dy = 1;
+    let dx = -1;
+    let dy = -1;
     socket.on("ballMove", () => {
-      if (this.ball.position[0] + dx > this.stage.w / 2) {
-        dx = -1;
-      }
-
       if (
         this.ball.position[0] + dx < -this.stage.w / 2 ||
-        this.ball.position[1] + dy > this.stage.h / 2
-      ) {
-        dx = 1;
-        this.ball.position = [3, 3, 1];
-      }
-      // if (this.ball.position[1] + dy > this.stage.h / 2) {
-      //   dy = -1;
-      // }
+        this.ball.position[0] + dx > this.stage.w / 2
+      )
+        dx *= -1;
 
-      if (this.ball.position[1] + dy < -this.stage.h / 2) {
-        dy = 1;
-      }
+      if (
+        this.ball.position[1] + dy < -this.stage.h / 2 ||
+        this.ball.position[1] + dy > this.stage.h / 2
+      )
+        this.ball.position = [3, 3, 1];
+
       this.ball.position[0] += 0.05 * dx;
       this.ball.position[1] += 0.05 * dy;
       socket.emit("ballMove", this.ball.position);
